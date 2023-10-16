dd72061a93c443813418386b9345869ddb95b60c
wkycode <misterwky@gmail.com>
2023-10-16 13:28:23 +0800

    clean up

--- server.ts
+++ server.ts
@@ -1,13 +1,7 @@
 import express from "express";
-
-const app = express();
-
-// app.use('/', (async () => await import('./src/routes.js')))
-app.use(express.urlencoded({ extended: true })); // parses url queries to req.query
-app.use(express.json()); // parses json to req.body
-// app.use('/', routes)
-
-app.use("/", (await import("./src/routes.ts")).default);
 const PORT = 8080;
-
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/", (await import("./src/routes.ts")).default);
 app.listen(PORT, () => console.log(`Server Running on port ${PORT}`));
